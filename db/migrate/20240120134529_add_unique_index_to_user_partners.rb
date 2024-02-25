e59fad87efbc793cdf33d4a56cb3e7e3031841ca
堺虎太郎 <k29saka1@gmail.com>
2024-02-26 00:36:29 +0900

    migratefile_change

--- db/migrate/20240120134529_add_unique_index_to_user_partners.rb
+++ db/migrate/20240120134529_add_unique_index_to_user_partners.rb
@@ -1,5 +1,5 @@
 class AddUniqueIndexToUserPartners < ActiveRecord::Migration[7.0]
   def change
-    add_index :user_partners, [:follower_id, :followed_id], unique: true
+    #add_index :user_partners, [:follower_id, :followed_id], unique: true
   end
 end
